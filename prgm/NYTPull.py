433bef7db5ff14a0420fb61371fabc970298d220
unknown <Harris_B@230477LT-OEUS.ad.psb.bls.gov>
2015-10-05 16:06:39 -0400

    Update 3

--- prgm/NYTPull.py
+++ prgm/NYTPull.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+##goal = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?\
+##        fq=glocations%3A%28%22Russia%22+%22Iran%22%29&begin_date=20100101&\
+##        end_date=20130101&sort=oldest&page=1&\
+##        api-key=KEYGOESHERE'.replace(' ','')
+
 import json
 import requests
 import os
@@ -9,33 +14,44 @@ class NYTPull(object):
     def __init__(self):
         pass
 
+    def ReplaceString(self, string):
+        find = {':':'%3A','"':'%22','(':'%28',')':'%29',' ':'+'}
+        for key in find:
+            string = string.replace(key, find[key])
+        return string
+
     def URL(self, key):
-        base_URL = 'http://api.nytimes.com/svc/search/v2/articlesearch'
-        fq = 'glocations:("Russia")'
+        base_url = 'http://api.nytimes.com/svc/search/v2/articlesearch'
+        resp_format = 'json'
+        fq = self.ReplaceString('glocations:("Russia" "Iran")')
         begin_date = '20100101'
-        end_date = '20120101'
+        end_date = '20130101'
         sort = 'oldest'
         page = '{}'
-        key = key
-        return ''.join([base_URL, '.json?fq=', fq, \
+        key = self.ReplaceString(key)
+        return ''.join([base_url, '.', resp_format, \
+                        '?fq=', fq, \
                         '&begin_date=', begin_date, \
-                        '&end_date=', end_date, '&sort=', sort, \
-                        '&page=', page, '&api-key=', key])
+                        '&end_date=', end_date, \
+                        '&sort=', sort, '&page=', page, \
+                        '&api-key=', key])
                         
-    def FetchURL(self):
-        URL = URL()
+    def FetchURL(self, key):
+        URL = self.URL(key).format(1)
         r = requests.get(URL)
         payload = r.json()
-        return payload
+        return payload['response']
         
-    def SavePull(self):
-        data = FetchURL()
+    def SavePull(self, key):
+        data = self.FetchURL(key)
+        content = json.dumps(data)
+        with open('test{}.json'.format(1), 'w') as f:
+            f.write(content)
         
-        
-        
-
 if __name__ == "__main__":
     c = NYTPull()
-    key = 'key'
-    url = c.URL(key)
+    key = '3b0e8a2c16c2aabe3a3ca8b76ef573fc:1:72949871'
+    url = c.URL(key).format(1)
     print(url)
+    c.SavePull(key)
+
