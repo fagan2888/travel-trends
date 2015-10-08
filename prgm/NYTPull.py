b8d9b12da70142805145f37088dc2f65c347f94c
unknown <Harris_B@230477LT-OEUS.ad.psb.bls.gov>
2015-10-08 16:46:30 -0400

    Update 6

--- prgm/NYTPull.py
+++ prgm/NYTPull.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-##goal = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?\
-##        fq=glocations%3A%28%22Russia%22+%22Iran%22%29&begin_date=20100101&\
-##        end_date=20130101&sort=oldest&page=1&\
-##        api-key=KEYGOESHERE'.replace(' ','')
+##AND section_name:("World" "Travel" "Foreign")
 
 import json
 import requests
@@ -21,14 +18,15 @@ class NYTPull(object):
             string = string.replace(key, find[key])
         return string
 
-    def URL(self, key):
+    def FormatCountryList(self, country_list):
+        pass
+
+    def URL(self, country, begin_date, end_date, key):
         base_url = 'http://api.nytimes.com/svc/search/v2/articlesearch'
         resp_format = 'json'
-        fq = self.ReplaceString('glocations:("Russia" "Iran")')
-        begin_date = '20100101'
-        end_date = '20130101'
+        fq = self.ReplaceString('glocations:({})'.format(country))
         sort = 'oldest'
-        page = '{}'
+        if page>100: raise ValueError('Page > 100: Needs to be <=100')
         key = self.ReplaceString(key)
         return ''.join([base_url, '.', resp_format, \
                         '?fq=', fq, \
@@ -37,27 +35,26 @@ class NYTPull(object):
                         '&sort=', sort, '&page=', page, \
                         '&api-key=', key])
                         
-    def FetchURL(self, key, i):
-        URL = self.URL(key).format(i)
-        r = requests.get(URL)
-        payload = r.json()
-        return payload['response']
+    def FetchURL(self, url):
+        r = requests.get(url)
+        if r.status_code == 200:
+            payload = r.json()
+            return payload['response'] 
         
-    def SavePull(self, key, i):
-        data = self.FetchURL(key, i)
+    def SavePull(self, data):
         content = json.dumps(data)
         with open('Page{}.json'.format(i), 'w') as f:
             f.write(content)
-        if i == 1: return json.dumps(data['meta']['hits'])
 
     def Main(self):
-        key = '3b0e8a2c16c2aabe3a3ca8b76ef573fc:1:72949871'
-        x = c.SavePull(key, 1)
-        print('START')
-        time.sleep(10)
-        print(x)
+        country = ''
+        begin_date = ''
+        end_date = ''
+        key = 'XXXXXXX'
+        url = c.URL(country, begin_date, end_date, key)
+        data = FetchURL(url)
+        c.SavePull(data)
         
 if __name__ == "__main__":
     c = NYTPull()
     c.Main()
-
